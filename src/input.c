60ecdb698f4d1bd50366f0ed8df991aa4ca4ecdd
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2017-03-29 13:44:41 +0200

    [Render] Fixed cursor placement after move

--- src/input.c
+++ src/input.c
@@ -1,13 +1,21 @@
 #include <stdlib.h>
-#include <ncurses.h>
 #include <stdbool.h>
+#include <ncurses.h>
 
 #include "input.h"
 #include "util.h"
 
 void input_loop(gap_T buff) {
     while (true) {
+        /* int x, y; */
         gap_buffer_print_curses(buff);
+
+        /* getmaxyx(stdscr, y, x); */
+        /* int row = buff->cursor / y+1; */
+        /* int col = buff->cursor-(row*(y+1)); */
+        /* move(row, col); */
+        /* refresh(); */
+
         handle_insertion(buff);
         erase();
     }
@@ -21,14 +29,15 @@ void handle_insertion(gap_T buff) {
     case '\n':
         gap_buffer_put(buff, c);
         break;
+    case 127:
     case KEY_BACKSPACE:
         gap_buffer_delete(buff);
         break;
     case KEY_LEFT:
-        getyx(stdscr, x, y);
+        gap_buffer_move_cursor(buff, -1);
+        getyx(stdscr, y, x);
         move(y, x-1);
         refresh();
-        gap_buffer_move_cursor(buff, -1);
         break;
     case 'q':
         exit(1);
--- src/util.c
+++ src/util.c
@@ -1,8 +1,8 @@
 /* useful functions that make sens only in the context of this project */
 #include <ncurses.h>
-#include <util.h>
+#include "util.h"
+#include "lib/gap_buffer.h"
 
-// TODO: print should set the (ncurses) cursor to its appropriate place
 void gap_buffer_print_curses(gap_T g) {
     for (int i = 0 ; i <= g->end ; ++i) {
         if (i < g->gap_start || i > g->gap_end) {
@@ -11,4 +11,12 @@ void gap_buffer_print_curses(gap_T g) {
             }
         }
     }
+    int x, y;
+    getmaxyx(stdscr, y, x);
+
+    // TODO: adjust for \n at the end of the line
+    int row = g->cursor / (x+1);
+    int col = g->cursor-(row*(x+1));
+
+    move(row, col);
 }
